72433b9778fc983770145388298cbd97c245bae6
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-08-04 18:35:40 +0100

    directives object correctly passed

--- myApp.js
+++ myApp.js
@@ -15,8 +15,10 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(
   helmet.contentSecurityPolicy({
-    defaultSrc: ["'self'"],
-    scriptSrc: "trusted-cdn.com",
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
   })
 );
 
